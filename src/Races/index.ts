53ea1af2d9887c54c4bf142ac8230e7db9b13a2f
lucas <lucmaieski@gmail.com>
2022-04-11 17:57:14 -0300

    Feito requisto 2

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,9 @@
 import Race from "./Race";
+import Dwarf from "./Dwarf/Dwarf";
+import Elf from "./Elf/Elf";
+import Halfling from "./Halfling/Halfling";
+import Orc from "./Orc/Orc";
 
 export default Race;
+
+export { Dwarf, Elf, Halfling, Orc };
